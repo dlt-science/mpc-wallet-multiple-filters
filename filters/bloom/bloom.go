15be77f6eb8ed2d3f709abc0322f203b5a185859
walter-hernandez <52283659+walter-hernandez@users.noreply.github.com>
2023-10-24 18:15:05 +0100

    Added bloom filter implementation in Go

---
+++ filters/bloom/bloom.go
@@ -0,0 +1,21 @@
+// Documentation:
+// https://pkg.go.dev/github.com/bits-and-blooms/bloom/v3
+
+package main
+
+import (
+	"fmt"
+	"github.com/bits-and-blooms/bloom/v3"
+)
+
+func main() {
+
+	// Create a new Bloom Filter with 1,000,000 items and 0.01% false positive rate
+	bf := bloom.NewWithEstimates(1000000, 0.01)
+	bf.Add([]byte("Hello, world!"))
+
+	// Test for existence (false positive)
+	if bf.Test([]byte("Hello, world!")) {
+		fmt.Println("Exists!")
+	}
+}
--- go.mod
+++ go.mod
@@ -13,6 +13,8 @@ require (
 )
 
 require (
+	github.com/bits-and-blooms/bitset v1.10.0 // indirect
+	github.com/bits-and-blooms/bloom/v3 v3.6.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
